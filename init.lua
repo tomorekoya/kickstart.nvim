96d316445865e06e85da2aebf91a0f58d3d5b228
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2024-07-28 15:19:52 -0400

    Use tmux-navigator package
    - Integrate neovim better with tmux

--- init.lua
+++ init.lua
@@ -980,6 +980,10 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
