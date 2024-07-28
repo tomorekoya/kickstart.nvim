c26f2d7ff9a422b8ea9252d65de3ad1be976dfba
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2024-07-28 16:19:11 -0400

    Add JDT LS configuration settins (basic)

--- init.lua
+++ init.lua
@@ -984,6 +984,7 @@ require('lazy').setup({
     'christoomey/vim-tmux-navigator',
     lazy = false,
   },
+  { 'mfussenegger/nvim-jdtls' },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
